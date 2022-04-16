3dc8050b91803f33402891866d84a520e3ff7f62
Fatimah Emad El-Den <63984422+xxfatj@users.noreply.github.com>
2022-04-16 23:52:07 +0300

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -1,4 +1,9 @@
 import pandas as pd
 
 pd.DateFrames =[{"Unit1": "Pandas}, {"unit2":, "Series"}];
+                
+                 
+myData = pd.DataFrame({'day2': ['two hours', 'three hours'], 'Day2': ['two hours', 'two hours'], index=['sunday', 'monday'] })
+                
+myDate             
                  
